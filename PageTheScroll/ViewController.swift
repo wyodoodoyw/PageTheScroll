de0bf5dc833a71710a92191fc5121327cc0cf9d0
wyodoodoyw <mwood051@gmail.com>
2016-10-06 16:42:43 -0400

    smaller scrollView
    small scrollView so you can see upcoming icons

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -17,7 +17,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
         var contentWidth: CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
@@ -26,13 +31,14 @@ class ViewController: UIViewController {
             
             var newX: CGFloat = 0.0
             
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
             contentWidth += newX
             
             scrollView.addSubview(imageView)
-            imageView.frame = CGRect(x: (newX - 75), y: ((view.frame.size.height / 2) - 75), width: 150, height: 150)
+            imageView.frame = CGRect(x: (newX - 75), y: ((scrollView.frame.size.height / 2) - 75), width: 150, height: 150)
         }
-        
+        // scrollView.backgroundColor = UIColor.purple
+        scrollView.clipsToBounds = false
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
