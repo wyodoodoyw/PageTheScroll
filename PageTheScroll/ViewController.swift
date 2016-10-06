4461166b4cff2d8607ca2876f8b3bbc3db98e536
wyodoodoyw <mwood051@gmail.com>
2016-10-06 16:29:14 -0400

    First scrollView component
    Added first scrollView component, doesn’t scroll

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     var images = [UIImageView]()
 
     override func viewDidLoad() {
@@ -17,7 +19,14 @@ class ViewController: UIViewController {
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
-            images.append(UIImageView(image: image))
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: (newX - 75), y: ((view.frame.size.height / 2) - 75), width: 150, height: 150)
         }
         
     }
