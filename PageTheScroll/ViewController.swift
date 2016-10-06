f97c097725fc81e27d7543ff43bd58c3a4ba9e9b
wyodoodoyw <mwood051@gmail.com>
2016-10-06 16:32:39 -0400

    Functional scrollView
    scrollView functions

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -17,18 +17,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var contentWidth: CGFloat = 0.0
+        
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
             let imageView = UIImageView(image: image)
             images.append(imageView)
             
             var newX: CGFloat = 0.0
+            
             newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            contentWidth += newX
             
             scrollView.addSubview(imageView)
             imageView.frame = CGRect(x: (newX - 75), y: ((view.frame.size.height / 2) - 75), width: 150, height: 150)
         }
         
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
 }
