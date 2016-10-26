7ca002e47bdfbc19d61209c5fef38a36aaa9ac4d
wyodoodoyw <mwood051@gmail.com>
2016-10-26 16:27:44 -0400

    implemented swipegesturerecogniser

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     
     var images = [UIImageView]()
+    var currentPage = 0
+    let MAX_PAGE = 2
+    let MIN_PAGE = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,5 +45,33 @@ class ViewController: UIViewController {
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
+    @IBAction func detectSwipe (_ sender: UISwipeGestureRecognizer) {
+        if (currentPage < MAX_PAGE && sender.direction == UISwipeGestureRecognizerDirection.left) {
+            moveScrollView(direction: 1)
+            
+        }
+        
+        if (currentPage > MIN_PAGE && sender.direction == UISwipeGestureRecognizerDirection.right) {
+            moveScrollView(direction: -1)
+        }
+    }
+    
+    func moveScrollView(direction: Int){
+        currentPage = currentPage + direction
+        let point: CGPoint = CGPoint(x: scrollView.frame.size.width * CGFloat(currentPage), y: 0.0)
+        scrollView.setContentOffset(point, animated: true)
+        
+//        // Create a animation to increase the actual icon on screen
+//        UIView.animate(withDuration: 0.4){
+//            self.images[self.currentPage].transform = CGAffineTransform.init(scaleX: 1.4, y: 1.4)
+//            
+//            // Revert icon size of the non-active pages
+//            for x in 0..<self.images.count {
+//                if (x != self.currentPage) {
+//                    self.images[x].transform = CGAffineTransform.identity
+//                }
+//            }
+//        }
+    }
 }
 
